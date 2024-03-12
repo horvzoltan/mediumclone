782ebee3278b436842d92a7981128ce7ee823373
horvzoltan <zoltanhorvath0721@gmail.com>
2024-03-12 20:24:30 +0100

    tt

--- src/app/auth/components/register/register/register.component.ts
+++ src/app/auth/components/register/register/register.component.ts
@@ -1,21 +1,20 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from "@angular/forms";
-import {Store} from "@ngrx/store";
-import {register} from "../../../store/actions";
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { register } from '../../../store/actions';
 
 @Component({
   selector: 'mc-register',
-  standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  standalone: true,
+  imports: [ReactiveFormsModule],
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
     username: ['', Validators.required],
     email: ['', Validators.required],
     password: ['', Validators.required],
-  })
+  });
 
   constructor(private fb: FormBuilder, private store: Store) {
     console.log('form', this.form.getRawValue());
