d588cd398199f4ec123d14adef22ce1b9804c5a5
horvzoltan <Zoltan.Horvath2@dmgmori-heitec.hu>
2024-03-13 08:43:16 +0100

    fix import

--- src/app/auth/components/register/register/register.component.ts
+++ src/app/auth/components/register/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { register } from '../../../store/actions';
 
